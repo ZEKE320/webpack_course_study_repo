5b61e966f273f43a8c3c50fe104cb1ed1726cbef
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-08-19 17:54:15 +0900

    🔧 webpack.config.jsを追加

---
+++ test_env/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+/** @type {webpack.Configuration} */
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "./js/main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/main.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/template/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
