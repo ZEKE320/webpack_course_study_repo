9bd250051e92619c08f36e25fab4e39b2dda3219
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-08-20 02:29:01 +0900

    🎨 my.jsにES6形式の記述を追加

--- src/js/my.js
+++ src/js/my.js
@@ -1,3 +1,5 @@
 export default () => {
-  console.log("Called module my.js");
+  const obj = { a: 1, b: 2 };
+  const newObj = { ...obj, c: 3 };
+  console.log(`Called module my.js!!! newObj: ${JSON.stringify(newObj)}`);
 };
