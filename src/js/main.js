928292afdcdd41252e4ce3f276e557163681df90
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-08-23 17:43:25 +0900

    🧑‍💻 js, sassからsrcディレクトリを直接参照できるように変更

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import "./reactApp.jsx";
-import my from "./my.js";
-import "../sass/main.scss";
-import add from "../ts/add.ts";
+import "js/reactApp.jsx";
+import my from "js/my.js";
+import "sass/main.scss";
+import add from "ts/add.ts";
 
 console.log("Hello JS!");
 my();
