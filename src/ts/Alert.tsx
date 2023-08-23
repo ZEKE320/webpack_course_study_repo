ce25f78ca441b5126da7f46490a5211edf80c338
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-08-23 13:13:14 +0900

    💄 styled-componentsを用いたUIに変更

--- src/ts/Alert.tsx
+++ src/ts/Alert.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const AlertContainer = styled.div`
+  background-color: green;
+  color: #fff;
+  padding: 1em;
+`;
 
 const Alert: React.FC<{ message: string }> = ({ message }) => {
-  return (
-    <div style={{ backgroundColor: "green", color: "white", padding: "1em" }}>
-      {message}
-    </div>
-  );
+  return <AlertContainer>{message}</AlertContainer>;
 };
 
 export default Alert;
